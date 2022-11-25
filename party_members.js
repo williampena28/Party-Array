80036fd8a0125e604d75cfd661db89ac329249bc
williampena28 <56842242+williampena28@users.noreply.github.com>
2022-11-25 12:13:50 -0500

    first commit

---
+++ party_members.js
@@ -0,0 +1,30 @@
+let party = [
+    {
+        name: "Timothy",
+        hitpoints: 10,
+        belongings: ["sword", "potion", "Tums"],
+        companion: {
+            name: "Velma",
+            type: "bat",
+        }
+    },
+    {
+        name: "Joline",
+        hitpoitns: 15,
+        belongings: ["spear", "bread", "Tums"],
+        companion: {
+            name: "Tabby",
+            type: "cat",
+        }
+    },
+    {
+        name: "Sarah",
+        hitpoitns: 20,
+        belongings: ["bow", "arrows", "wood"],
+        companion: {
+            name: "Tony",
+            type: "tiger",
+        }
+    },
+]
+
